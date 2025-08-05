b87e63bd79c0a2231a924267878d3026d9acf688
lokeshfsg <sarakanamlokesh15@gmail.com>
2025-08-05 22:30:19 +0530

    Update first.js

--- response/first.js
+++ response/first.js
@@ -1,3 +1,8 @@
 document.getElementById("loginBtn").addEventListener("click", function() {
-   window.location.href = "/instagram_clone";
+   const email = document.getElementById("email").value;
+   const password = document.getElementById("password").value;
+   check = "lokesh15102001";
+   if(password===check) {
+      window.location.href = 'instagram_clone.html'
+   }
 })
